bf7c1a598b513feb921068c3493b58ba68b4b2a5
Stephanie Nwankwo <stephybelle208@gmail.com>
2023-10-28 05:44:59 +0100

    Added IdeaApis file.

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -1,34 +1,45 @@
+import IdeasApi from "../services/IdeasApi";
+
 class IdeaList {
   constructor() {
     this._ideaListEl = document.querySelector("#idea-list");
-    this._ideas = [
-      
-    ];
+    this._ideas = [];
+    this.getIdeas();
     this._validTags = new Set();
-    this._validTags.add('technology');
-    this._validTags.add('software');
-    this._validTags.add('business');
-    this._validTags.add('education');
-    this._validTags.add('health');
-    this._validTags.add('inventions');
+    this._validTags.add("technology");
+    this._validTags.add("software");
+    this._validTags.add("business");
+    this._validTags.add("education");
+    this._validTags.add("health");
+    this._validTags.add("inventions");
+  }
+
+  async getIdeas() {
+    try {
+      const res = await IdeasApi.getIdeas();
+      this._ideas = res.data.data;
+      this .render();
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   getTagClass(tag) {
     tag = tag.toLowerCase();
-    let tagClass = '';
+    let tagClass = "";
     if (this._validTags.has(tag)) {
-      tagClass = `tag-${tag}`
+      tagClass = `tag-${tag}`;
     } else {
-      tagClass = '';
+      tagClass = "";
     }
     return tagClass;
   }
 
   render() {
-
-    this._ideaListEl.innerHTML = this._ideas.map((idea) => {
-      const tagClass = this.getTagClass(idea.tag);
-      return `
+    this._ideaListEl.innerHTML = this._ideas
+      .map((idea) => {
+        const tagClass = this.getTagClass(idea.tag);
+        return `
       <div class="card">
           <button class="delete"><i class="fas fa-times"></i></button>
           <h3>
@@ -42,7 +53,8 @@ class IdeaList {
         </div>
       
       `;
-    }).join(''); 
+      })
+      .join("");
   }
 }
 
